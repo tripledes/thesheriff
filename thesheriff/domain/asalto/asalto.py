bc1d0484d8b3f9ed2e6cc9c02b355ffa624e549c
rmuhamed <ricardo.muhamed@gmail.com>
2019-11-23 19:53:57 +0100

    Domain classes added

---
+++ thesheriff/domain/asalto/asalto.py
@@ -0,0 +1,8 @@
+class Asalto:
+
+    def __init__(self, id, name, bandidos, sheriff):
+        self.bandidos = bandidos
+        self.score = 0
+        self.name = name
+        self.id = id
+        self.sheriff = sheriff
--- thesheriff/domain/banda/banda.py
+++ thesheriff/domain/banda/banda.py
@@ -1,3 +1,5 @@
 class Banda:
     def __init__(self, id):
         self.id = id
+        self.miembros = []
+        self.asaltosCreados = 0
