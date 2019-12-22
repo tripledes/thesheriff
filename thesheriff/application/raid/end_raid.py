abe93ff57a5796fdcb02d830bcc8759e5128a102
Sergi Jimenez <tripledes@users.noreply.github.com>
2019-12-22 08:58:59 +0100

    fix: ci green again (#71)

--- thesheriff/application/raid/end_raid.py
+++ thesheriff/application/raid/end_raid.py
@@ -10,12 +10,15 @@ class EndRaid:
         raid = self.raid_repository.of_id(raid_id=raid_id)
 
         if(not raid.could_finish()):
-            raise Exception('Raid can not be finished until all outlaws have ranked it.')
+            raise Exception(
+                'Raid can not be finished until all outlaws have ranked it.')
 
         score_gang = 0
         for outlaw in raid.outlaws:
             score_gang += outlaw.score
 
-        #FIXME: send email with gang updated score and sherrif's score in this raid
+        # FIXME: send email with gang updated score
+        # and sherrif's score in this raid
 
-        return "Gang's score: {}. Sheriff's score on raid '{}': {}".format(score_gang, raid.name, raid_score)
+        return "Gang's score: {}. Sheriff's score on raid '{}': {}".format(
+            score_gang, raid.name, raid_score)
